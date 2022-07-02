26ee1bf5c1175b14c550f10954595cdf910de17e
Bracth <bryanricardo.florencioabreu@gmail.com>
2022-07-02 15:27:35 +0200

    Added react routing to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import ArticulesList from "./components/articules/articulesList/ArticulesList.jsx";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World, Hello compis</h1>
-      <ArticulesList></ArticulesList>
+      <Routes>
+          <Route path="/" element={<Navigate to="/articules"/>}></Route>
+          <Route path="/articules">
+            <Route index element={<ArticulesList></ArticulesList>}></Route>
+          </Route>
+           <Route path="/404" element={<div> 404 | Not found Page</div>}></Route>
+          <Route path="*" element={<Navigate to="/404" />}></Route>
+      </Routes>
     </div>
   );
 }
