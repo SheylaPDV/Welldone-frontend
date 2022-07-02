28304bf22c5d6c4e76632d27e05fcec351adc14c
Bracth <bryanricardo.florencioabreu@gmail.com>
2022-07-02 15:17:52 +0200

    Added the ArticuleList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ArticulesList from "./components/articules/articulesList/ArticulesList.jsx";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World, Hello compis</h1>
+      <ArticulesList></ArticulesList>
     </div>
   );
 }
