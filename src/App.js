1eee6cbd350bdb13983a3d5709adae00f6f40547
Sheyla Perez <sheylapdv@gmail.com>
2022-07-16 10:53:06 +0200

    almacenando token en local storage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { AuthContextProvider } from "./components/login/context.js";
 import CreateUserPage from "./components/login/CreateUserPage.jsx";
 import LoginPage from "./components/login/LoginPage.jsx";
 
-function App({ isInitiallyLogged, className }) {
+function App({ isInitiallyLogged }) {
   const [isLogged, setIsLogged] = useState(isInitiallyLogged);
 
   const handleLogin = () => {
@@ -18,21 +18,21 @@ function App({ isInitiallyLogged, className }) {
   };
   return (
     <div className="App">
-      <Header />
       <AuthContextProvider value={{ isLogged, handleLogin, handleLogout }}>
+        <Header />
         <Routes>
           <Route path="/" element={<Navigate to="/articules" />}></Route>
           <Route path="/articules">
             <Route index element={<ArticulesList></ArticulesList>}></Route>
           </Route>
           <Route path="/createAccount" element={<CreateUserPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
 
           <Route path="/404" element={<div> 404 | Not found Page</div>}></Route>
           <Route path="*" element={<Navigate to="/404" />}></Route>
         </Routes>
+        <Footer />
       </AuthContextProvider>
-      <Footer />
     </div>
   );
 }
